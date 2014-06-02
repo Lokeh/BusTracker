d02f58724bfe73ebd04def2bcaf76de1395e2bd5
Will Acton <actonw.work@gmail.com>
2014-06-01 20:48:54 -0700

    AngularJS implementation, nav, panel logic in DOM

---
+++ app.js
@@ -0,0 +1,193 @@
+/* app.js */
+'use strict';
+
+//(function () { // Everything in a closure... cause why not?
+	/*********************** Trimet Logic ***********************/
+
+	function Arrival(stopID, callback) {
+		// AppID: 160D6AAA210C9D54156D56820
+		return $.ajax({
+			type: 'GET',
+			dataType: 'json',
+			//url: 'http://developer.trimet.org/ws/V1/arrivals?json=true&locIDs=6849,6850&appID=160D6AAA210C9D54156D56820',
+			url: 'http://developer.trimet.org/ws/V1/arrivals?json=true&appID=160D6AAA210C9D54156D56820&locIDs='+stopID,
+			success: function (data) {
+				if (callback !== undefined) {
+					callback (data);
+				}
+			}
+		});
+	}
+
+	function Location(long, lat, callback) {
+		return $.ajax({
+			type: 'GET',
+			dataType: 'json',
+			url: 'http://developer.trimet.org/ws/V1/stops?json=true&appID=160D6AAA210C9D54156D56820&ll='+long+','+lat,
+			success: function (data) {
+				if (callback !== undefined) {
+					callback (data);
+				}
+			}
+		});
+	}
+
+	function Route(routeIDs, callback) {
+		return $.ajax({
+			type: 'GET',
+			dataType: 'json',
+			url: 'http://developer.trimet.org/ws/V1/routeConfig?json=true&appID=160D6AAA210C9D54156D56820&routes='+routeIDs,
+			success: function (data) {
+				if (callback !== undefined) {
+					callback (data);
+				}
+			}
+		});
+	}
+
+	function Stops(routeID, callback) {
+		return $.ajax({
+			type: 'GET',
+			dataType: 'json',
+			url: 'http://developer.trimet.org/ws/V1/routeConfig?json=true&appID=160D6AAA210C9D54156D56820&dir=true&stops=true&routes='+routeID,
+			success: function (data) {
+				if (callback !== undefined) {
+					callback (data);
+				}
+			}
+		})
+	}
+
+	function getStopID(data, direction, num, callback) { // num - number of stop in chain
+		if (callback !== undefined) {
+			callback (data.resultSet.route[0].dir[direction].stop[num].locid);
+		}
+	}
+
+	function outputRoutes(data) {
+		data.resultSet.route.forEach (function (route) {
+			$("#routes").append('<li id="'+route.route+'">'+route.desc+'</li>');
+		});
+	}
+
+	function outputStops(data) {
+		var selector = "#"+data.resultSet.route[0].route;
+		appendStops(selector, data.resultSet.route[0].dir);
+		
+	}
+
+	function appendStops(selector, dir) {
+		dir.forEach(function (direction, i) {
+				//console.log(direction);
+				$(selector).append('<ul id="'+i+'"><strong>'+direction.desc)+'</strong>';
+				direction.stop.forEach (function (stop) {
+					$(selector+" #"+i).append('<li id="'+stop.locid+'">'+stop.desc+'</li>');
+				});
+				$(selector).append('</ul>');
+		});
+	}
+
+	function outputArrival(data) {
+		selector = "#"+data.resultSet.location[0].locid
+		console.log(selector);
+		$(selector).append("<ul>");
+		data.resultSet.arrival.forEach(function (arrival) {
+			$(selector+" ul").append('<li>'+arrival.scheduled+'</li>');
+		});
+		$(selector).append("</ul>")
+	}
+
+	/*$(document).ready(function () {
+		Route('', function (data) { outputRoutes(data) });
+		Stops(58, function (data) {
+			outputStops(data);
+			getStopID(data, 0, 6, function (id) {
+				Arrival(id, function (data) {
+					outputArrival(data);
+				});
+			});
+		});
+		Stops(6, function (data) { outputStops(data); })
+	});*/
+
+	/********************* End Trimet Logic *********************/
+
+	var app = angular.module('busTrackerApp', []);
+
+	app.filter('rails', function () {
+		return function (input) {
+			var trains = [];
+			input.forEach(function (route) {
+				if (route.type === 'R') {
+					trains.push(route);
+				}
+			});
+			return trains;
+		};
+	});
+
+	app.filter('bus', function () {
+		return function (input) {
+			var bus = [];
+			input.forEach(function (route) {
+				if (route.type === 'B' && route.desc.indexOf('Shuttle') === -1) {
+					bus.push(route);
+				}
+			});
+			return bus;
+		};
+	});
+
+	app.filter('shuttle', function () {
+		return function (input) {
+			var shuttle = [];
+			input.forEach(function (route){
+				if (route.desc.indexOf('Shuttle') > -1) {
+					shuttle.push(route);
+				}
+			});
+			return shuttle;
+		};
+	});
+
+	app.controller('RouteController', function ($scope) {
+		$scope.routes = [];
+
+		$scope.safeApply = function(fn) {
+		    var phase = this.$root.$$phase;
+		    if(phase == '$apply' || phase == '$digest') {
+		        if(fn && (typeof(fn) === 'function')) {
+		          fn();
+		        }
+		    } else {
+		       this.$apply(fn);
+		    }
+		};
+
+		$scope.getRoutes = function getRoutes() {
+			Route('', function (data) {
+				$scope.safeApply(function () {
+					$scope.routes = data.resultSet.route;
+				});
+			})
+		};
+
+		$scope.displayRoutes = function displayRoutes() {
+			console.log($scope.routes);
+		};
+
+		var init = function () {
+			$scope.getRoutes();
+		};
+
+		init();
+	});
+
+	/*Route('', function (data) {
+		app.controller('RouteController', function (data) {
+			this.routes = data.resultSet.route; // an array starting at 0 for each stop;
+		});
+	});*/
+
+
+//}) // End Closure
