7f577bbee65778d93036b3b44152c3826a0028c0
Will Acton <actonw.work@gmail.com>
2014-06-03 20:22:45 -0700

    index.php blargh

--- index.php
+++ index.php
@@ -1,63 +1,60 @@
 <!doctype html>
-<html ng-app="busTrackerApp">
+<!-- Routes View -->
+<html class="no-js" lang="en">
 	<head>
-		<meta name="viewport" content="width=device-width, user-scalable=no">
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Better Trimet Tracker</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>Foundation | Welcome</title>
+		<link rel="stylesheet" href="css/foundation.css" />
+		<link rel="stylesheet" href="style.css" />
 
 		<!-- Angular -->
 		<script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.2.15/angular.min.js"></script>
-
+		<script src="js/angular-ui-router.min.js"></script>
 		<script src="app.js"></script>
-		<!-- Bootstrap -->
-		<link href="css/bootstrap.min.css" rel="stylesheet">
-		<link href="http://netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
-		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-		<!--[if lt IE 9]>
-		  <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-		  <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-		<![endif]-->
-		<link rel="stylesheet" type="text/css" href="style.css">
+
+		<!-- Other -->
+		<script src="js/vendor/modernizr.js"></script>
 	</head>
-	<body>
-		<div class="container" ng-controller="RouteController as buses">
-			<h1>Bus Routes</h1>
-			<button type="button" ng-click="displayRoutes()">Debug</button>
-			<section ng-controller="RouteTypeController as routetype">
-				<ul class="nav nav-default nav-tabs">
-					<li ng-class="{ active: routetype.isSelected('all') }"><a href ng-click="routetype.selectTab('all')">All</a></li>
-					<li ng-class="{ active: routetype.isSelected('rails') }"><a href ng-click="routetype.selectTab('rails')">Trains</a></li>
-					<li ng-class="{ active: routetype.isSelected('bus') }"><a href ng-click="routetype.selectTab('bus')">Buses</a></li>
-					<li ng-class="{ active: routetype.isSelected('shuttle') }"><a href ng-click="routetype.selectTab('shuttle')">Shuttles</a></li>
-					<input type="text" ng-model="searchText">
+	<body ng-app="busTrackerApp">
+		<nav class="top-bar" data-topbar>
+			<ul class="title-area">
+				<li class="name">
+					<h1><a href="#">AppName</a></h1>
+				</li>
+				<!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
+				<li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
+			</ul>
+
+			<section class="top-bar-section">
+				<!-- Nav Section -->
+				<ul class="left">
+					<li class="active"><a href>Routes</a></li>
+					<li><a href>Stops</a></li>
+					<li><a href>Nearby</a></li>
 				</ul>
-				<div class="panel panel-default" ng-show="routetype.isSelected('all')">
-					<ul id="all">
-						<li ng-repeat="route in routes | filter:searchText"><a href id="{{ route.route }}"><i class="fa fa-chevron-right"></i> {{ route.desc }}</a></li>
-					</ul>
-				</div>
-				<div class="panel panel-default" ng-show="routetype.isSelected('rails')">
-					<ul id="rails">
-						<li ng-repeat="route in routes | rails | filter:searchText"><a href id="{{ route.route }}"><i class="fa fa-chevron-right"></i> {{ route.desc }}</a></li>
-					</ul>
-				</div>
-				<div class="panel panel-default" ng-show="routetype.isSelected('bus')">
-					<ul id="bus">
-						<li ng-repeat="route in routes | bus | filter:searchText"><a href id="{{ route.route }}"><i class="fa fa-chevron-right"></i> {{ route.desc }}</a></li>
-					</ul>
-				</div>
-				<div class="panel panel-default" ng-show="routetype.isSelected('shuttle')">
-					<ul id="shuttle"><i class="fa fa-chevron-right"></i> 
-						<li ng-repeat="route in routes | shuttle | filter:searchText"><a href id="{{ route.route }}"><i class="fa fa-chevron-right"></i> {{ route.desc }}</a></li>
-					</ul>
+				
+				<!-- Right Nav Section -->
+				<ul class="right">
+					<li><a href>About</a></li>
+				</ul>
+			</section>
+		</nav>
+		<ul class="breadcrumbs">
+			<li class="current"><a href>Routes</a></li>
+		</ul>
+		<section role="main">
+			<div class="row">
+				<div class="columns" ui-view>
+
 				</div>
 			</div>
 		</section>
-		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-		<script src="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
-		
+	
+		<script src="js/vendor/jquery.js"></script>
+		<script src="js/foundation.min.js"></script>
+		<script>
+		  $(document).foundation();
+		</script>
 	</body>
 </html>
