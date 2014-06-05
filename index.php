c2fdb2b79f6ad24ba500ea938813ceeeec1abca8
Will Acton <actonw.work@gmail.com>
2014-06-04 22:51:58 -0700

    index.php...sigh

--- index.php
+++ index.php
@@ -3,9 +3,10 @@
 <html class="no-js" lang="en">
 	<head>
 		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scaleable=no" />
 		<title>Welcome</title>
 		<link rel="stylesheet" href="css/foundation.css" />
+		<link href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
 		<link rel="stylesheet" href="style.css" />
 
 		<!-- Angular -->
